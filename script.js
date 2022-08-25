5cc6f8547db3250de27b4ce1dfd84544e2a289f2
Carlos Iego <carlosiego2017@gmail.com>
2022-08-25 14:45:12 -0300

    hoje

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function entrada_caixa1(){
   var pcaixa = document.getElementById('primeiracaixa').value
   var scaixa = document.getElementById('segundacaixa').value
 
-  // Conversão: Quilometro para ...
+  // Objetos de calculos de conversão
   var dic_quilo = {
     'Metro': pcaixa*1000,
     'Centímetro': pcaixa*100000,
@@ -73,16 +73,100 @@ function entrada_caixa1(){
     'Milha náutica': pcaixa / 1852
   }
 
-    if(selectdois_txt == 'Quilometro'){
-      var dic_afazer = dic_quilo
-    }else if (selectdois_txt == 'Metro'){
-      var dic_afazer = dic_metro
+  var dic_cent = {
+    'Quilometro': pcaixa / 100000,
+    'Metro': pcaixa / 100,
+    'Milímetro': pcaixa * 10,
+    'Micrômeto': pcaixa * 10000,
+    'Nanômetro': pcaixa * 1e+7,
+    'Milha': pcaixa / 160900,
+    'Jarda': pcaixa / 91.44,
+    'Pé': pcaixa / 30.48, 
+    'Polegada': pcaixa / 2.54,
+    'Milha náutica': pcaixa / 185200
+
+  }
+
+  var dic_mili = {
+    'Quilometro': pcaixa / 1e+6,
+    'Metro': pcaixa / 1000,
+    'Centímetro': pcaixa / 10,
+    'Micrômeto': pcaixa * 1000,
+    'Nanômetro': pcaixa * 1e+6,
+    'Milha': pcaixa / 1.609e+6,
+    'Jarda': pcaixa / 914.4,
+    'Pé': pcaixa / 304.8, 
+    'Polegada': pcaixa / 25.4,
+    'Milha náutica': pcaixa / 1.852e+6
+  }
+
+  var dic_micro = {
+    'Quilometro': pcaixa / 1e+9,
+    'Metro': pcaixa / 1e+6,
+    'Centímetro': pcaixa / 10000,
+    'Milímetro': pcaixa / 1000,
+    'Nanômetro': pcaixa * 1000,
+    'Milha': pcaixa / 1.609e+9,
+    'Jarda': pcaixa / 914400,
+    'Pé': pcaixa / 304800, 
+    'Polegada': pcaixa / 25400,
+    'Milha náutica': pcaixa / 1.852e+9
+  }
+  
+  var dic_nano = {
+    'Quilometro': pcaixa / 1e+12,
+    'Metro': pcaixa / 1e+9,
+    'Centímetro': pcaixa / 1e+7,
+    'Milímetro': pcaixa / 1e+6,
+    'Micrômeto': pcaixa / 1000,
+    'Milha': pcaixa / 1.609e+12,
+    'Jarda': pcaixa /  9.144e+8,
+    'Pé': pcaixa / 3.048e+8, 
+    'Polegada': pcaixa / 2.54e+7,
+    'Milha náutica': pcaixa / 1.852e+12
+  }
+
+  var dic_milha = {
+    'Quilometro': pcaixa * 1.609,
+    'Metro': pcaixa * 1609,
+    'Centímetro': pcaixa * 160900,
+    'Milímetro': pcaixa * 1.609e+6,
+    'Micrômeto': pcaixa * 1.609e+9,
+    'Nanômetro': pcaixa / 1.609e+12,
+    'Jarda': pcaixa * 1760,
+    'Pé': pcaixa * 5280, 
+    'Polegada': pcaixa * 63360,
+    'Milha náutica': pcaixa / 1.151
+  }
+
+  var dic_jarda = {
+    'Quilometro': pcaixa / 1094,
+    'Metro': pcaixa / 1.094,
+    'Centímetro': pcaixa * 160900,
+    'Milímetro': pcaixa * 1.609e+6,
+    'Micrômeto': pcaixa * 1.609e+9,
+    'Nanômetro': pcaixa / 1.609e+12,
+    'Jarda': pcaixa * 1760,
+    'Pé': pcaixa * 5280, 
+    'Polegada': pcaixa * 63360,
+    'Milha náutica': pcaixa / 1.151
+  }
+
+  var dic_med_gerais = {'Quilometro': dic_quilo, 'Metro': dic_metro, 'Centímetro': dic_cent, 'Milímetro': dic_mili, 'Micrômeto': dic_micro, 'Nanômetro': dic_nano, 'Milha': dic_milha, 'Jarda': dic_jarda}
+
+  // Verifica option selecionado na segunda caixa
+  for(var element in dic_med_gerais){
+    if(selectdois_txt == element){
+      var dic_afazer = dic_med_gerais[element]
     }
-    for(var pos in dic_afazer){
-      if(pos == selecttres_txt){
-        $('#segundacaixa').attr('value', `${dic_afazer[pos]}`)
-      }
+  }
+
+  // Verifica option selecionado na terceira caixa
+  for(var medida in dic_afazer){
+    if(medida == selecttres_txt){
+      $('#segundacaixa').attr('value', `${dic_afazer[medida]}`)
     }
+  }
   
 }
 
