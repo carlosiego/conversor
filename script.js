ed9d0fe7d26e7cffee13e8dc22427c333ec603fa
Carlos Iego <carlosiego2017@gmail.com>
2022-08-27 18:14:52 -0300

    f

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function escolhap(){
 }
 
 
-function entrada_caixa1(n){
+function entrada_caixa(n){
   
   var selectdois = document.getElementById('select2')
   var selectdois_txt = selectdois.options[selectdois.selectedIndex].text
@@ -46,189 +46,192 @@ function entrada_caixa1(n){
   var pcaixa = document.getElementById('primeiracaixa').value
   var scaixa = document.getElementById('segundacaixa').value
 
-  var caixa_entrada = Number
+  var caixa_entrada_valor = Number
   var caixa_saida = ''
-  if(n == 1){
-    caixa_entrada = pcaixa
+
+  var selectp = ''
+  var selects = ''
+
+  if(n == 1 || n == 4){
     caixa_saida = '#segundacaixa'
-    
-  }else if(n == 2){
-    caixa_entrada = scaixa
+    caixa_entrada_valor = pcaixa
+
+  }else if(n == 2 || n == 3){
     caixa_saida = '#primeiracaixa'
+    caixa_entrada_valor = scaixa
   }
   // Objetos de calculos de conversão de Comprimento
+
   var dic_quilo = {
-    'Metro': caixa_entrada*1000,
-    'Centímetro': caixa_entrada*100000,
-    'Milímetro': caixa_entrada*1e+6,
-    'Micrômeto': caixa_entrada*1e+9,
-    'Nanômetro': caixa_entrada*1e+12,
-    'Milha': caixa_entrada / 1.609,
-    'Jarda': caixa_entrada * 1094,
-    'Pé': caixa_entrada * 3281, 
-    'Polegada': caixa_entrada * 39370,
-    'Milha náutica': caixa_entrada / 1.852
+    'Metro': caixa_entrada_valor*1000,
+    'Centímetro': caixa_entrada_valor*100000,
+    'Milímetro': caixa_entrada_valor*1e+6,
+    'Micrômeto': caixa_entrada_valor*1e+9,
+    'Nanômetro': caixa_entrada_valor*1e+12,
+    'Milha': caixa_entrada_valor / 1.609,
+    'Jarda': caixa_entrada_valor * 1094,
+    'Pé': caixa_entrada_valor * 3281, 
+    'Polegada': caixa_entrada_valor * 39370,
+    'Milha náutica': caixa_entrada_valor / 1.852
   }
 
   var dic_metro = {
-    'Quilometro': caixa_entrada / 1000,
-    'Centímetro': caixa_entrada * 100,
-    'Milímetro': caixa_entrada * 1000,
-    'Micrômeto': caixa_entrada * 1e+6,
-    'Nanômetro': caixa_entrada * 1e+9,
-    'Milha': caixa_entrada / 1609,
-    'Jarda': caixa_entrada * 1.094,
-    'Pé': caixa_entrada * 3.281, 
-    'Polegada': caixa_entrada * 39.37,
-    'Milha náutica': caixa_entrada / 1852
+    'Quilometro': caixa_entrada_valor / 1000,
+    'Centímetro': caixa_entrada_valor * 100,
+    'Milímetro': caixa_entrada_valor * 1000,
+    'Micrômeto': caixa_entrada_valor * 1e+6,
+    'Nanômetro': caixa_entrada_valor * 1e+9,
+    'Milha': caixa_entrada_valor / 1609,
+    'Jarda': caixa_entrada_valor * 1.094,
+    'Pé': caixa_entrada_valor * 3.281, 
+    'Polegada': caixa_entrada_valor * 39.37,
+    'Milha náutica': caixa_entrada_valor / 1852
   }
 
   var dic_cent = {
-    'Quilometro': caixa_entrada / 100000,
-    'Metro': caixa_entrada / 100,
-    'Milímetro': caixa_entrada * 10,
-    'Micrômeto': caixa_entrada * 10000,
-    'Nanômetro': caixa_entrada * 1e+7,
-    'Milha': caixa_entrada / 160900,
-    'Jarda': caixa_entrada / 91.44,
-    'Pé': caixa_entrada / 30.48, 
-    'Polegada': caixa_entrada / 2.54,
-    'Milha náutica': caixa_entrada / 185200
+    'Quilometro': caixa_entrada_valor / 100000,
+    'Metro': caixa_entrada_valor / 100,
+    'Milímetro': caixa_entrada_valor * 10,
+    'Micrômeto': caixa_entrada_valor * 10000,
+    'Nanômetro': caixa_entrada_valor * 1e+7,
+    'Milha': caixa_entrada_valor / 160900,
+    'Jarda': caixa_entrada_valor / 91.44,
+    'Pé': caixa_entrada_valor / 30.48, 
+    'Polegada': caixa_entrada_valor / 2.54,
+    'Milha náutica': caixa_entrada_valor / 185200
 
   }
 
   var dic_mili = {
-    'Quilometro': caixa_entrada / 1e+6,
-    'Metro': caixa_entrada / 1000,
-    'Centímetro': caixa_entrada / 10,
-    'Micrômeto': caixa_entrada * 1000,
-    'Nanômetro': caixa_entrada * 1e+6,
-    'Milha': caixa_entrada / 1.609e+6,
-    'Jarda': caixa_entrada / 914.4,
-    'Pé': caixa_entrada / 304.8, 
-    'Polegada': caixa_entrada / 25.4,
-    'Milha náutica': caixa_entrada / 1.852e+6
+    'Quilometro': caixa_entrada_valor / 1e+6,
+    'Metro': caixa_entrada_valor / 1000,
+    'Centímetro': caixa_entrada_valor / 10,
+    'Micrômeto': caixa_entrada_valor * 1000,
+    'Nanômetro': caixa_entrada_valor * 1e+6,
+    'Milha': caixa_entrada_valor / 1.609e+6,
+    'Jarda': caixa_entrada_valor / 914.4,
+    'Pé': caixa_entrada_valor / 304.8, 
+    'Polegada': caixa_entrada_valor / 25.4,
+    'Milha náutica': caixa_entrada_valor / 1.852e+6
   }
 
   var dic_micro = {
-    'Quilometro': caixa_entrada / 1e+9,
-    'Metro': caixa_entrada / 1e+6,
-    'Centímetro': caixa_entrada / 10000,
-    'Milímetro': caixa_entrada / 1000,
-    'Nanômetro': caixa_entrada * 1000,
-    'Milha': caixa_entrada / 1.609e+9,
-    'Jarda': caixa_entrada / 914400,
-    'Pé': caixa_entrada / 304800, 
-    'Polegada': caixa_entrada / 25400,
-    'Milha náutica': caixa_entrada / 1.852e+9
+    'Quilometro': caixa_entrada_valor / 1e+9,
+    'Metro': caixa_entrada_valor / 1e+6,
+    'Centímetro': caixa_entrada_valor / 10000,
+    'Milímetro': caixa_entrada_valor / 1000,
+    'Nanômetro': caixa_entrada_valor * 1000,
+    'Milha': caixa_entrada_valor / 1.609e+9,
+    'Jarda': caixa_entrada_valor / 914400,
+    'Pé': caixa_entrada_valor / 304800, 
+    'Polegada': caixa_entrada_valor / 25400,
+    'Milha náutica': caixa_entrada_valor / 1.852e+9
   }
   
   var dic_nano = {
-    'Quilometro': caixa_entrada / 1e+12,
-    'Metro': caixa_entrada / 1e+9,
-    'Centímetro': caixa_entrada / 1e+7,
-    'Milímetro': caixa_entrada / 1e+6,
-    'Micrômeto': caixa_entrada / 1000,
-    'Milha': caixa_entrada / 1.609e+12,
-    'Jarda': caixa_entrada /  9.144e+8,
-    'Pé': caixa_entrada / 3.048e+8, 
-    'Polegada': caixa_entrada / 2.54e+7,
-    'Milha náutica': caixa_entrada / 1.852e+12
+    'Quilometro': caixa_entrada_valor / 1e+12,
+    'Metro': caixa_entrada_valor / 1e+9,
+    'Centímetro': caixa_entrada_valor / 1e+7,
+    'Milímetro': caixa_entrada_valor / 1e+6,
+    'Micrômeto': caixa_entrada_valor / 1000,
+    'Milha': caixa_entrada_valor / 1.609e+12,
+    'Jarda': caixa_entrada_valor /  9.144e+8,
+    'Pé': caixa_entrada_valor / 3.048e+8, 
+    'Polegada': caixa_entrada_valor / 2.54e+7,
+    'Milha náutica': caixa_entrada_valor / 1.852e+12
   }
 
   var dic_milha = {
-    'Quilometro': caixa_entrada * 1.609,
-    'Metro': caixa_entrada * 1609,
-    'Centímetro': caixa_entrada * 160900,
-    'Milímetro': caixa_entrada * 1.609e+6,
-    'Micrômeto': caixa_entrada * 1.609e+9,
-    'Nanômetro': caixa_entrada / 1.609e+12,
-    'Jarda': caixa_entrada * 1760,
-    'Pé': caixa_entrada * 5280, 
-    'Polegada': caixa_entrada * 63360,
-    'Milha náutica': caixa_entrada / 1.151
+    'Quilometro': caixa_entrada_valor * 1.609,
+    'Metro': caixa_entrada_valor * 1609,
+    'Centímetro': caixa_entrada_valor * 160900,
+    'Milímetro': caixa_entrada_valor * 1.609e+6,
+    'Micrômeto': caixa_entrada_valor * 1.609e+9,
+    'Nanômetro': caixa_entrada_valor / 1.609e+12,
+    'Jarda': caixa_entrada_valor * 1760,
+    'Pé': caixa_entrada_valor * 5280, 
+    'Polegada': caixa_entrada_valor * 63360,
+    'Milha náutica': caixa_entrada_valor / 1.151
   }
 
   var dic_jarda = {
-    'Quilometro': caixa_entrada / 1094,
-    'Metro': caixa_entrada / 1.094,
-    'Centímetro': caixa_entrada * 160900,
-    'Milímetro': caixa_entrada * 1.609e+6,
-    'Micrômeto': caixa_entrada * 1.609e+9,
-    'Nanômetro': caixa_entrada / 1.609e+12,
-    'Jarda': caixa_entrada * 1760,
-    'Pé': caixa_entrada * 5280, 
-    'Polegada': caixa_entrada * 63360,
-    'Milha náutica': caixa_entrada / 1.151
+    'Quilometro': caixa_entrada_valor / 1094,
+    'Metro': caixa_entrada_valor / 1.094,
+    'Centímetro': caixa_entrada_valor * 160900,
+    'Milímetro': caixa_entrada_valor * 1.609e+6,
+    'Micrômeto': caixa_entrada_valor * 1.609e+9,
+    'Nanômetro': caixa_entrada_valor / 1.609e+12,
+    'Jarda': caixa_entrada_valor * 1760,
+    'Pé': caixa_entrada_valor * 5280, 
+    'Polegada': caixa_entrada_valor * 63360,
+    'Milha náutica': caixa_entrada_valor / 1.151
   }
 
   var dic_pe = {
-    'Quilometro': caixa_entrada / 3281,
-    'Metro': caixa_entrada / 3.281,
-    'Centímetro': caixa_entrada * 30.48,
-    'Milímetro': caixa_entrada * 304.8,
-    'Micrômeto': caixa_entrada * 304800,
-    'Nanômetro': caixa_entrada / 3.048e+8,
-    'Jarda': caixa_entrada / 3,
-    'Milha': caixa_entrada / 5280, 
-    'Polegada': caixa_entrada * 12,
-    'Milha náutica': caixa_entrada / 6076
+    'Quilometro': caixa_entrada_valor / 3281,
+    'Metro': caixa_entrada_valor / 3.281,
+    'Centímetro': caixa_entrada_valor * 30.48,
+    'Milímetro': caixa_entrada_valor * 304.8,
+    'Micrômeto': caixa_entrada_valor * 304800,
+    'Nanômetro': caixa_entrada_valor / 3.048e+8,
+    'Jarda': caixa_entrada_valor / 3,
+    'Milha': caixa_entrada_valor / 5280, 
+    'Polegada': caixa_entrada_valor * 12,
+    'Milha náutica': caixa_entrada_valor / 6076
   }
 
   var dic_poleg = {
-    'Quilometro': caixa_entrada / 39370,
-    'Metro': caixa_entrada / 39.37,
-    'Centímetro': caixa_entrada * 2.54,
-    'Milímetro': caixa_entrada * 25.4,
-    'Micrômeto': caixa_entrada * 25400,
-    'Nanômetro': caixa_entrada / 2.54e+7,
-    'Jarda': caixa_entrada / 36,
-    'Milha': caixa_entrada / 63360, 
-    'Pé': caixa_entrada / 12,
-    'Milha náutica': caixa_entrada / 72910
+    'Quilometro': caixa_entrada_valor / 39370,
+    'Metro': caixa_entrada_valor / 39.37,
+    'Centímetro': caixa_entrada_valor * 2.54,
+    'Milímetro': caixa_entrada_valor * 25.4,
+    'Micrômeto': caixa_entrada_valor * 25400,
+    'Nanômetro': caixa_entrada_valor / 2.54e+7,
+    'Jarda': caixa_entrada_valor / 36,
+    'Milha': caixa_entrada_valor / 63360, 
+    'Pé': caixa_entrada_valor / 12,
+    'Milha náutica': caixa_entrada_valor / 72910
   }
 
   var dic_milhaN = {
-    'Quilometro': caixa_entrada / 1.852,
-    'Metro': caixa_entrada / 1852,
-    'Centímetro': caixa_entrada * 185200,
-    'Milímetro': caixa_entrada * 1.852e+6,
-    'Micrômeto': caixa_entrada * 1.852e+9,
-    'Nanômetro': caixa_entrada / 1.852e+12,
-    'Jarda': caixa_entrada * 2025,
-    'Milha': caixa_entrada * 1.151, 
-    'Pé': caixa_entrada * 6076,
-    'Polegada': caixa_entrada * 72910
+    'Quilometro': caixa_entrada_valor / 1.852,
+    'Metro': caixa_entrada_valor / 1852,
+    'Centímetro': caixa_entrada_valor * 185200,
+    'Milímetro': caixa_entrada_valor * 1.852e+6,
+    'Micrômeto': caixa_entrada_valor * 1.852e+9,
+    'Nanômetro': caixa_entrada_valor / 1.852e+12,
+    'Jarda': caixa_entrada_valor * 2025,
+    'Milha': caixa_entrada_valor * 1.151, 
+    'Pé': caixa_entrada_valor * 6076,
+    'Polegada': caixa_entrada_valor * 72910
   }
 
   var dic_med_gerais = {'Quilometro': dic_quilo, 'Metro': dic_metro, 'Centímetro': dic_cent, 'Milímetro': dic_mili, 'Micrômeto': dic_micro, 'Nanômetro': dic_nano, 'Milha': dic_milha, 'Jarda': dic_jarda, 'Pé': dic_pe, 'Polegada': dic_poleg, 'Milha náutica': dic_milhaN}
 
-  var teste = document.querySelector('#teste')
+  var dic_afazer = {}
+
   // Verificando qual caixa de entrada e definindo a caixa de saida
-
-  if(n == 2){
-    caixa_entrada = scaixa
-    caixa_saida = '#primeiracaixa'
-    for(var ele in dic_med_gerais){
-      teste.innerHTML += `${ele} -` 
-  }
-}
-
-  if(caixa_entrada.length != 0){
+  if(caixa_entrada_valor.length != 0){
     if(selectdois_txt == selecttres_txt){
-      $(`${caixa_saida}`).attr('value', `${pcaixa * 1}`)
+      $(`${caixa_saida}`).attr('value', `${caixa_entrada_valor}`)
     }
-    
     // Verifica option selecionado na segunda caixa
+    if(n == 1 || n == 4){
+      selectp = selectdois_txt
+      selects = selecttres_txt
+    }else if(n == 2 || n == 3){
+      selectp = selecttres_txt
+      selects = selectdois_txt
+    }
     for(var element in dic_med_gerais){
-      if(selectdois_txt == element){
-        var dic_afazer = dic_med_gerais[element]
+      if(selectp == element){
+        dic_afazer = dic_med_gerais[element]
       }
     }
   
     // Verifica option selecionado na terceira caixa
     for(var medida in dic_afazer){
-      if(medida == selecttres_txt){
+      if(medida == selects){
         $(`${caixa_saida}`).attr('value', `${dic_afazer[medida]}`)
       }
     }
